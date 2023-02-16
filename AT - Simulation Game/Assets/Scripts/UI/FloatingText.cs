418fea34f5a4ef4f3bf9c3925fa2d261b0ea6459
Norbert Kupeczki <norbert2.kupeczki@live.uwe.ac.uk>
2023-02-16 14:41:14 +0000

    Asset folder restructure + added fade-out effect to floating text

--- AT - Simulation Game/Assets/Scripts/UI/FloatingText.cs
+++ AT - Simulation Game/Assets/Scripts/UI/FloatingText.cs
@@ -8,16 +8,22 @@ public class FloatingText : MonoBehaviour
 {
     [SerializeField] float _speed = 20.0f;
     [SerializeField] float _lifeTime = 3.0f;
+    [SerializeField] [Range(0.01f, 1.0f)] float _fadeRate = 1.0f;
     [SerializeField] float _startingY = 0.0f;
     [SerializeField] string _name = "Floating Text";
 
     private RectTransform _rectTransform;
     private Vector3 _moveVector;
+    private TextMeshProUGUI _text;
+    private float _timer;
 
     private void Awake()
     {
         name = "[Floating text] " + _name;
+
         _rectTransform = GetComponent<RectTransform>();
+        _text = GetComponent<TextMeshProUGUI>();
+
         _rectTransform.localPosition = new Vector3(0.0f, _startingY, 0.0f);
         _moveVector = new Vector3(0.0f, -_speed, 0.0f);
     }
@@ -31,12 +37,16 @@ public class FloatingText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(_lifeTime > 0.0f)
+        _rectTransform.localPosition -= _moveVector * Time.deltaTime;
+        _timer += Time.deltaTime;
+
+        if(_lifeTime < _timer)
         {
-            _rectTransform.localPosition -= _moveVector * Time.deltaTime;
-            _lifeTime -= Time.deltaTime;
+            double fade = 0.02f/((0.8 * (_timer - _lifeTime) * Time.deltaTime * -_fadeRate) - 0.02f) + 1;
+            _text.alpha = Mathf.Lerp(_text.alpha, 0.0f, (float)fade);
         }
-        else
+
+        if (_text.alpha < 0.05f)
         {
             Destroy(gameObject);
         }
